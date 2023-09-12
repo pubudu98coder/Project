99c6580ca6b8536672e9190a6cc016f674a430a2
Binishi-0822 <binishipiyumika3@gmail.com>
2023-09-13 00:59:14 +0530

    Adding load page into final project

--- Form1/Program.cs
+++ Form1/Program.cs
@@ -16,7 +16,7 @@ namespace Form1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form6());
            //Application.Run(new Form2());
         }
     }
